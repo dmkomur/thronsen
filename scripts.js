89b6738fab8c38aff4370a1465b8e1f321cfd5c6
dmkomur <125804681+dmkomur@users.noreply.github.com>
2026-01-10 20:07:22 +0100

    feat: services section

--- scripts.js
+++ scripts.js
@@ -9,3 +9,33 @@ buttonOpen.addEventListener("click", () => {
 buttonClose.addEventListener("click", () => {
     mobileMenu.classList.remove("active");
 });
+
+const accordionHeaders = document.querySelectorAll(".accordion-header");
+
+accordionHeaders.forEach((header) => {
+    header.addEventListener("click", () => {
+        const openItem = document.querySelector(".accordion-content.open");
+        const content = header.nextElementSibling;
+        const arrow = header.querySelector(".arrow");
+
+        // Закрываем предыдущий, если это не текущий
+        if (openItem && openItem !== content) {
+            openItem.style.maxHeight = null;
+            openItem.classList.remove("open");
+            openItem.previousElementSibling
+                .querySelector(".arrow")
+                .classList.remove("open");
+        }
+
+        // Переключаем текущий
+        if (content.classList.contains("open")) {
+            content.style.maxHeight = null;
+            content.classList.remove("open");
+            arrow.classList.remove("open");
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+            content.classList.add("open");
+            arrow.classList.add("open");
+        }
+    });
+});
